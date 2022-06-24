6a45f11592501f88877e5bb6eac4b90c3e1f3d15
Doridian <git@doridian.net>
2022-06-23 19:52:22 -0700

    a

--- shared/sockets/groups/base.go
+++ shared/sockets/groups/base.go
@@ -12,12 +12,13 @@ type SocketGroup struct {
 
 	macTable    map[shared.MacAddr]*sockets.Socket
 	socketTable map[*sockets.Socket]shared.MacAddr
-	macLock     sync.RWMutex
+	macLock     *sync.RWMutex
 }
 
 func MakeSocketGroup() *SocketGroup {
 	return &SocketGroup{
 		AllowClientToClient: false,
 		macTable:            make(map[shared.MacAddr]*sockets.Socket),
+		macLock:             &sync.RWMutex{},
 	}
 }

1a2beb026c7134d04294c78dc7286d5197f5ce12
Doridian <git@doridian.net>
2022-08-07 13:40:30 -0700

    reuse workflow

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 #!/usr/bin/env pwsh
+
 docker build -t wsvpn-test -f Dockerfile.test .
 docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -it wsvpn-test $args

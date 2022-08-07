cf640334a94f528c54888cfc5411e121d9a0ec24
Doridian <git@doridian.net>
2022-08-07 14:04:20 -0700

    rm t

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
-docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -it ghcr.io/doridian/wsvpn/testcontainer:latest "$@"
+docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest "$@"

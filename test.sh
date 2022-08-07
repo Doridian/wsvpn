973dbc373f57bd755fad22882e32baa2b1727e53
Doridian <git@doridian.net>
2022-08-07 16:23:40 -0700

    adjust test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
+echo "GITHUB_ACTIONS = $GITHUB_ACTIONS"
+
 docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
 docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest "$@"

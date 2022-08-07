2d651888c3e71fe5f7bcb5643bf38c727ee08d74
Doridian <git@doridian.net>
2022-08-07 13:51:32 -0700

    Make testcontainer a folder and job etc

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-docker build -t wsvpn-test -f Dockerfile.test .
+docker build -t wsvpn-test ./testcontainer
 docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -it wsvpn-test "$@"

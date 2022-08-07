b97e8e9293160398662dd47a549a587bb89c9640
Doridian <git@doridian.net>
2022-08-06 21:23:57 -0700

    WebTransport test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,10 @@
 from tests.bins import GoBin
 from tests.conftest import TLSCertSet
 
+def basic_traffic_test(svbin: GoBin, clbin: GoBin) -> None:
+    # TODO: Actually send back and forth some packets here...
+    pass
+
 def test_run_e2e_wss(svbin: GoBin, clbin: GoBin, tls_cert_server: TLSCertSet) -> None:
     svbin.cfg["server"]["tls"] = {
         "key": tls_cert_server.key,
@@ -18,9 +22,33 @@ def test_run_e2e_wss(svbin: GoBin, clbin: GoBin, tls_cert_server: TLSCertSet) ->
     clbin.start()
     clbin.assert_ready_ok()
 
+    basic_traffic_test(svbin=svbin, clbin=clbin)
+
     clbin.stop()
     svbin.stop()
 
+def test_run_e2e_webtransport(svbin: GoBin, clbin: GoBin, tls_cert_server: TLSCertSet) -> None:
+    svbin.cfg["server"]["tls"] = {
+        "key": tls_cert_server.key,
+        "certificate": tls_cert_server.cert,
+    }
+    svbin.cfg["server"]["enable-http3"] = True
+
+    clbin.cfg["client"]["tls"] = {
+        "ca": tls_cert_server.ca,
+    }
+    clbin.cfg["client"]["server"] = "webtransport://127.0.0.1:9000"
+
+    svbin.start()
+    svbin.assert_ready_ok()
+
+    clbin.start()
+    clbin.assert_ready_ok()
+
+    basic_traffic_test(svbin=svbin, clbin=clbin)
+
+    clbin.stop()
+    svbin.stop()    
 
 def test_run_server(svbin: GoBin) -> None:
     svbin.start()
@@ -33,6 +61,8 @@ def test_run_e2e_base(svbin: GoBin, clbin: GoBin) -> None:
 
     clbin.start()
     clbin.assert_ready_ok()
+
+    basic_traffic_test(svbin=svbin, clbin=clbin)
 
     clbin.stop()
     svbin.stop()

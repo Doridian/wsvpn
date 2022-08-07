6ebdec371c27a19b14254cf80da9bd29ab762af8
Doridian <git@doridian.net>
2022-08-07 13:34:37 -0700

    reduce privileges of test docker

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 docker build -t wsvpn-test -f Dockerfile.test .
-docker run --privileged -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt" -it wsvpn-test $args
+docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -it wsvpn-test $args
--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t wsvpn-test -f Dockerfile.test .
-docker run --privileged -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt" -it wsvpn-test "$@"
+docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -it wsvpn-test "$@"

2409d14b0251812610836b4e2cd564b8473beedf
Mark Dietzer <git@doridian.net>
2022-08-17 20:36:23 -0700

    ensure to run test container with --rm (#66)

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 
 docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
-docker run --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest $args
+docker run --rm --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest $args
--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -e
 echo "GITHUB_ACTIONS = $GITHUB_ACTIONS"
 
 docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
-docker run -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest "$@"
+docker run --rm -e GITHUB_ACTIONS --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest "$@"

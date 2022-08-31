f1571ab2f0c0bacb69c557321b750e2ceeb79582
Mark Dietzer <git@doridian.net>
2022-08-31 08:52:46 -0700

    More IPv6 tests (#86)

--- tests/test_multiclient.py
+++ tests/test_multiclient.py
@@ -4,6 +4,7 @@ from tests.bins import GoBin, new_clbin
 from tests.packet_utils import basic_traffic_test
 import pytest
 
+
 @pytest.fixture(scope="function")
 def clbin2() -> Generator:
     gobin = new_clbin()
@@ -11,22 +12,22 @@ def clbin2() -> Generator:
     gobin.stop()
 
 
-def runtest(svbin: GoBin, clbins: list, one_iface: bool, mode: str) -> None:
+def runtest(svbin: GoBin, clbins: list, one_iface: bool, mode: str, ip_version: int) -> None:
     svbin.cfg["interface"]["one-interface-per-connection"] = one_iface
     svbin.cfg["tunnel"]["mode"] = mode
+    svbin.ip_version = ip_version
 
     if mode == "TAP" and not svbin.is_tap_supported():
         pytest.skip("TAP not supported on this platform")
 
     if one_iface and not svbin.is_one_interface_per_connection_supported(mode):
-        pytest.skip("One-Interface-Per-Connection not supported on this platform")
-
+        pytest.skip(
+            "One-Interface-Per-Connection not supported on this platform")
 
     if get_local_platform() == "windows":
         svbin.cfg["interface"]["name"] = "%s0" % mode
         for i, clbin in enumerate(clbins):
             clbin.cfg["interface"]["name"] = "%s%d" % (mode, (i + 1))
-
 
     try:
         for clbin in clbins:
@@ -40,7 +41,7 @@ def runtest(svbin: GoBin, clbins: list, one_iface: bool, mode: str) -> None:
             clbin.assert_ready_ok()
 
         for clbin in clbins:
-            basic_traffic_test(svbin=svbin, clbin=clbin)
+            basic_traffic_test(svbin=svbin, clbin=clbin, ip_version=ip_version)
 
     finally:
         for clbin in clbins:
@@ -48,21 +49,37 @@ def runtest(svbin: GoBin, clbins: list, one_iface: bool, mode: str) -> None:
 
 
 def test_run_e2e_oneiface_tap(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
-    runtest(svbin, [clbin, clbin2], False, "TAP")
+    runtest(svbin, [clbin, clbin2], False, "TAP", 4)
 
 
 def test_run_e2e_oneiface_tun(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
-    runtest(svbin, [clbin, clbin2], False, "TUN")
+    runtest(svbin, [clbin, clbin2], False, "TUN", 4)
 
 
 def test_run_e2e_manyiface_tap(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
-    runtest(svbin, [clbin, clbin2], True, "TAP")
+    runtest(svbin, [clbin, clbin2], True, "TAP", 4)
 
 
 def test_run_e2e_manyiface_tun(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
-    runtest(svbin, [clbin, clbin2], True, "TUN")
+    runtest(svbin, [clbin, clbin2], True, "TUN", 4)
 
 
 def test_run_e2e_manyiface_customname(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
     svbin.cfg["interface"]["name"] = "utun6"
-    runtest(svbin, [clbin, clbin2], True, "TUN")
+    runtest(svbin, [clbin, clbin2], True, "TUN", 4)
+
+
+def test_run_e2e_oneiface_ipv6_tap(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
+    runtest(svbin, [clbin, clbin2], False, "TAP", 6)
+
+
+def test_run_e2e_oneiface_ipv6_tun(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
+    runtest(svbin, [clbin, clbin2], False, "TUN", 4)
+
+
+def test_run_e2e_manyiface_ipv6_tap(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
+    runtest(svbin, [clbin, clbin2], True, "TAP", 6)
+
+
+def test_run_e2e_manyiface_ipv6_tun(svbin: GoBin, clbin: GoBin, clbin2: GoBin) -> None:
+    runtest(svbin, [clbin, clbin2], True, "TUN", 6)

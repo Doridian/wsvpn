5f7eeb93fc3d655ddf79c7988b960cd60a12ed09
Doridian <git@doridian.net>
2022-09-04 23:26:19 -0700

    Alwaya build testcontainer only locally

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 
-docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
-docker run --rm --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest $args
+docker build -t wsvpn-testcontainer testcontainer
+docker run --rm --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "${pwd}:/mnt:ro" -i wsvpn-testcontainer $args
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-docker pull ghcr.io/doridian/wsvpn/testcontainer:latest
-docker run --rm --sysctl net.ipv6.conf.default.disable_ipv6=0 --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i ghcr.io/doridian/wsvpn/testcontainer:latest "$@"
+docker build -t wsvpn-testcontainer testcontainer
+docker run --rm --sysctl net.ipv6.conf.default.disable_ipv6=0 --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun -v "/dev/net/tun:/dev/net/run" -v "$(pwd):/mnt:ro" -i wsvpn-testcontainer "$@"

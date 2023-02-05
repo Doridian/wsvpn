22b988a87618fc6f82379b3653425a3b3755c170
Doridian <git@doridian.net>
2023-02-04 18:49:50 -0800

    Actually, nah

--- shared/cli/flags.go
+++ shared/cli/flags.go
@@ -2,11 +2,6 @@ package cli
 
 import (
 	"flag"
-	"log"
-	"os"
-	"os/signal"
-	"runtime/pprof"
-	"syscall"
 )
 
 func LoadFlags(configName string, configHelp string) (*string, *bool) {
@@ -15,28 +10,10 @@ func LoadFlags(configName string, configHelp string) (*string, *bool) {
 	}
 	configPtr := flag.String("config", configName, configHelp)
 	printDefaultConfigPtr := flag.Bool("print-default-config", false, "Print default config to console")
-	cpuProfPtr := flag.String("cpuprofile", "", "CPU profile output file")
 
 	flag.Usage = UsageWithVersion
 
 	flag.Parse()
-
-	cpuProf := *cpuProfPtr
-	if cpuProf != "" {
-		f, err := os.Create(cpuProf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		sig := make(chan os.Signal, 1)
-		signal.Notify(sig, syscall.SIGUSR1)
-		go func() {
-			for {
-				<-sig
-				pprof.StopCPUProfile()
-			}
-		}()
-	}
 
 	return configPtr, printDefaultConfigPtr
 }

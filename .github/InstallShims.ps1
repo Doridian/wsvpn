b4fbb1ce81537ca0f6ac877b0928b7ec42a68acb
Mark Dietzer <git@doridian.net>
2023-04-10 21:46:33 -0700

    Make it all one test task and just shim (#202)

---
+++ .github/InstallShims.ps1
@@ -0,0 +1,14 @@
+New-Item -Force -Path $PROFILE.CurrentUserCurrentHost -ItemType file -Value @'
+function sudo(
+        [switch]$E,
+        [switch]$H,
+        [parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$Passthrough
+) {
+    Invoke-Expression "& $Passthrough"
+}
+
+function which($Value) {
+    Write-Output $Value
+}
+'@

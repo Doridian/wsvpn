af645e9458420f009e6abedc0e61d1059b9dfd4b
Doridian <git@doridian.net>
2024-01-07 12:26:27 -0800

    Use SyscallConn directly

--- client/clients/connection_linux.go
+++ client/clients/connection_linux.go
@@ -7,7 +7,7 @@ import (
 	"errors"
 	"log"
 	"net"
-	"os"
+	"syscall"
 
 	"golang.org/x/sys/unix"
 )
@@ -18,17 +18,15 @@ var ErrUnknownConnType = errors.New("not a known conn type")
 
 func setFirewallMark(conn net.Conn, mark int) error {
 	var err error
-	var file *os.File
+	var syscallConn syscall.Conn
 
 	switch typedConn := conn.(type) {
-	case *net.TCPConn:
-		file, err = typedConn.File()
-	case *net.UDPConn:
-		file, err = typedConn.File()
+	case syscall.Conn:
+		file = typedConn
 	case *tls.Conn:
 		return setFirewallMark(typedConn.NetConn(), mark)
 	default:
-		log.Printf("Unknown conn type: %T / %v", typedConn, typedConn)
+		log.Printf("Unknown conn type: %T (%v)", typedConn, typedConn)
 		err = ErrUnknownConnType
 	}
 
@@ -37,7 +35,7 @@ func setFirewallMark(conn net.Conn, mark int) error {
 	}
 
 	var operr error
-	fd, err := file.SyscallConn()
+	fd, err := syscallConn.SyscallConn()
 	if err != nil {
 		return err
 	}

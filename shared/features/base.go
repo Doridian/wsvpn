d4bef0013fa3e74b27e38a93a557ec3c88507b10
Doridian <git@doridian.net>
2024-04-27 16:42:11 -0700

    Just completely remove this feature

--- shared/features/base.go
+++ shared/features/base.go
@@ -4,14 +4,13 @@ type Feature = string
 
 const (
 	Fragmentation Feature = "fragmentation"
-	DatagramID0   Feature = "datagram_id_0"
 	Compression   Feature = "compression"
 )
 
 type Config = map[Feature]bool
 
 func IsFeatureSupported(feat Feature) bool {
-	return feat == Fragmentation || feat == DatagramID0
+	return feat == Fragmentation
 }
 
 type Container interface {
